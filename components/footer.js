99e5d4e114f1f6943f5a14e02e8fa9049f9512d8
Derrick Xu <derrickxu@Derricks-MBP.nyc.rr.com>
2023-01-25 21:46:21 -0500

    intergrate amplify

---
+++ components/footer.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { render } from "react-dom";
+import SocialFlow from "./SocialFlow";
+
+const footer = () => {
+    return ( 
+        <div>
+            Derrick Xu's Home page
+            <SocialFlow />
+        </div>
+
+     );
+}
+ 
+export default footer;
