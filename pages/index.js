a229e53e7a8f70fa83268afd8fd148c90e578766
DerrickXu1998 <dyuxu01@gmail.com>
2023-02-26 17:05:11 -0500

    revert

--- pages/index.js
+++ pages/index.js
@@ -14,32 +14,39 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi my name is Derrick Xu. A passionate software engineer based out of NYC!</p>
-        <p>
-          This is my personal home page to record my thinking, work and interests!
-        </p>
+        <p>Hi my name is Derrick Xu. A software engineer based out of NYC!</p>
+        <p> I am interested in topics of data modeling, NLP and of software engineering. Sometimes also entrepruner at heart. </p>
+        <p> This is my personal home page to record my thoughts, work and interests! which will COME OUT SOON</p>
+        <p>I also love to provide career advice to people who are interested in Tech!</p>
+        <p> I am happy to chat and connect</p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>{title}</Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
           ))}
         </ul>
-      </section>
+      </section> */}
+
+      {/* <section className={utilStyles.insideButton}>
+        <div>
+          <Footer />
+        </div>
+      </section> */}
     </Layout>
   );
 }
