1f5766813369f65e8d68c622dd3e9424814ec190
DerrickXu1998 <dyuxu01@gmail.com>
2023-02-26 17:25:26 -0500

    change site title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Derrick Xu';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Derrick Yufan Xu| SWE';
 
 export default function Layout({ children, home }) {
   return (
